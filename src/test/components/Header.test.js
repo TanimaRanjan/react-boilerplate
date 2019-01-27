08688a1e7df5df3933e887252092043deab36945
Tanima Ranjan <tanimaranjan@gmail.com>
2019-01-27 09:40:00 -0500

    Init

---
+++ src/test/components/Header.test.js
@@ -0,0 +1,16 @@
+// import ReachShallowRenderer from 'react-test-renderer/shallow'
+import React from 'react'
+import Header from '../../components/Header'
+import {shallow} from 'enzyme'
+import toJSON from 'enzyme-to-json'
+
+
+test('Should render Header correctly ', () => {
+    // const wrapper = shallow(<Header />)
+    // expect(toJSON(wrapper)).toMatchSnapshot()
+        // expect(wrapper.find('h1').length).toBe(1)
+        // expect(wrapper.find('h1').text()).toBe("Expense App")
+        // const renender = new ReachShallowRenderer();
+        // renender.render(<Header />)
+        // expect(renender.getRenderOutput()).toMatchSnapshot()
+})
